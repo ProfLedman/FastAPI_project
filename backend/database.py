9ff6d22560a805f00da2d03d7b0f3afed29bc803
Carlos <104063831+ProfLedman@users.noreply.github.com>
2025-09-26 17:37:05 +0300

    tweak

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URL = "postgresql://postgres:ledama@localhost:5432/fastapi_db"
+DB_URL = "postgresql://postgres:******@localhost:5432/fastapi_db"
 engine = create_engine(DB_URL)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
